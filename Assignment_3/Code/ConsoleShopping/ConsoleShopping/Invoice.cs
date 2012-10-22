2a1d577caca19c39a05a64efde5e4c6b86f8794f
ramc <ramc@maqsoftware.com>
2012-10-22 20:39:28 +0530

    Adding project with files to be excluded

---
+++ Assignment_3/Code/ConsoleShopping/ConsoleShopping/Invoice.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleShopping
+{
+    /// <summary>
+    /// Invoice is a collection of Shopping Items and calculates the value for each product and also the 
+    /// grand total value of the invoice.
+    /// It exposes an enumerator for outside classes to get information about the invoice line items, while
+    /// protecting the data from external changes.
+    /// </summary>
+    class Invoice : IEnumerator<InvoiceItem>, IEnumerable<InvoiceItem>
+    {
+        #region Private data members
+        private int invoiceItemIndex;
+        private List<InvoiceItem> shoppingCart;
+        private int invoiceValue;
+        #endregion
+        #region Constructors
+        public Invoice(List<InvoiceItem> shoppingCart)
+        {
+            invoiceItemIndex = -1;
+            this.shoppingCart = shoppingCart;
+            if ((shoppingCart != null) && (shoppingCart.Count > 0))
+            {
+                foreach (InvoiceItem invoiceItem in shoppingCart)
+                {
+                    invoiceValue += invoiceItem.InvoiceItemPrice;
+                }
+            }
+        }
+        #endregion
+        #region Public properties
+        public int InvoiceValue
+        {
+            get
+            {
+                return invoiceValue;
+            }
+        }
+        #endregion
+        #region IEnumerator implementation
+        public InvoiceItem Current
+        {
+            get { return shoppingCart[invoiceItemIndex]; }
+        }
+        public void Dispose()
+        {
+        }
+        object System.Collections.IEnumerator.Current
+        {
+            get { return Current; }
+        }
+        public bool MoveNext()
+        {
+            invoiceItemIndex++;
+            return (invoiceItemIndex < shoppingCart.Count);
+        }
+        public void Reset()
+        {
+            invoiceItemIndex = -1;
+        }
+        #endregion
+        #region IEnumerable implementation
+        public IEnumerator<InvoiceItem> GetEnumerator()
+        {
+            return (this as IEnumerator<InvoiceItem>);
+        }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        #endregion
+    }
+}
---
+++ Assignment_3/Code/ConsoleShopping/ConsoleShopping/ShoppingItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace ConsoleShopping
+{
+    /// <summary>
+    /// The basic encapsulation of an item available for sale.
+    /// It encapsulates the product id, product name and unit price.
+    /// </summary>
+    class ShoppingItem : IEqualityComparer<ShoppingItem>
+    {
+        #region Constructors
+        public ShoppingItem()
+        {
+            ItemNo = 0;
+            ItemName = string.Empty;
+            ItemPrice = 0;
+        }
+        public ShoppingItem(int itemNo, string itemName, int itemPrice)
+        {
+            ItemNo = itemNo;
+            ItemName = itemName;
+            ItemPrice = itemPrice;
+        }
+        #endregion
+        #region Public properties
+        public int ItemNo { get; private set; }
+        public string ItemName { get; private set; }
+        public int ItemPrice { get; private set; }
+        #endregion
+        #region IEqualityComparer<ShoppingItem> implementation
+        public bool Equals(ShoppingItem x, ShoppingItem y)
+        {
+            return (x.ItemNo == y.ItemNo);
+        }
+
+        public int GetHashCode(ShoppingItem obj)
+        {
+            return obj.ItemNo;
+        }
+        #endregion
+    }
+}
