782e48ff1ced7a8fe92e95bfdb6de7758783555f
ramc <ramc@maqsoftware.com>
2012-10-29 14:22:58 +0530

    Commiting on private branch

--- WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/Program.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApplication2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World! Hello Everyone");
         }
     }
 }
